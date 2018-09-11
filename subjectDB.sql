dd2c1727928e77ea7f7f6023f3e917e5196f148c
DESKTOP-RB5MSRC\清華 <matsuosayaka@gmail.com>
2018-09-11 16:51:11 +0900

    SQL userテーブル

---
+++ subjectDB.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE user (id SERIAL PRIMARY KEY, login_id varchar(255) UNIQUE Not Null, name varchar(255) Not Null, birth_date DATE Not Null,password Varchar(255) Not Null,create_date DATETIME Not Null,update_date DATETIME Not Null);
+INSERT INTO user VALUES (1, 'admin', '管理者', 19990911,'password',20180905,20180911);
+
